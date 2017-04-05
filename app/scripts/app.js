f6246df0b42635af6215122eb18697511d9fabf7
Renee Bain <rbain02@gmail.com>
2017-04-05 18:57:03 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function(){
+  function config($stateProvider, $locationProvider){
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+      $stateProvider
+        .state('rooms', {
+          url: '/',
+          controller: 'RoomCtrl as rooms',
+          templateUrl: '/templates/rooms.html'
+        });
+    }
+
+    angular
+    .module('blocChat', ['ui.router', 'firebase'])
+    .config(config);
+})();
